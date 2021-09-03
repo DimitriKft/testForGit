8deb0a71f04d94786abc24ae1fd58f999299127b
Dimitri Klopfstein <dimitri.kft@gmail.com>
2021-09-03 14:23:53 +0200

    first

---
+++ testForGitHub/testForGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  testForGitHubApp.swift
+//  testForGitHub
+//
+//  Created by dimitri on 03/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct testForGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
