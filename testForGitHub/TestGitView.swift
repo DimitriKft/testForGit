ba6e03fac8ff704fcb163ac28bfd47a9d42a4289
Dimitri Klopfstein <dimitri.kft@gmail.com>
2021-09-03 14:54:24 +0200

    test

---
+++ testForGitHub/TestGitView.swift
@@ -0,0 +1,23 @@
+//
+//  TestGitView.swift
+//  testForGitHub
+//
+//  Created by dimitri on 03/09/2021.
+//
+
+import SwiftUI
+
+struct TestGitView: View {
+    var body: some View {
+        VStack {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            Image(systemName: "person")
+        }
+    }
+}
+
+struct TestGitView_Previews: PreviewProvider {
+    static var previews: some View {
+        TestGitView()
+    }
+}
